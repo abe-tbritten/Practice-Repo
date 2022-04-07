20f12f03b94da526d62008a5dc4893e43f4e7d06
Britten <abe_tbritten@iastate.edu>
2022-04-07 14:59:33 -0500

    Initializing main branch

---
+++ Script_2.py
@@ -0,0 +1,9 @@
+import random
+names = {
+    'Thomas': [1, 2, 3],
+    'Max': [2, 3, 4],
+    'Emma': [3, 4, 5],
+    'Ally': [4, 5, 6]
+}
+n = random.randint(0, 3)
+print(names)
---
+++ Script_3.py
@@ -0,0 +1,5 @@
+import random
+a = random.randint(1, 25)
+b = random.randint(1, 25)
+print("a: " + str(a) + "\nb: " + str(b))
+print("a * b: " + str(a * b))
